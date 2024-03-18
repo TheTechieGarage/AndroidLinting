ae5dfd4df147bd99c4f7ac9750af325923bd7c2c
Annie Kobia <kobia.annunziata@kyosk.app>
2024-03-18 23:41:08 +0300

    Integrate detekt

--- app/src/main/java/com/anniekobia/androidlinting/MainActivity.kt
+++ app/src/main/java/com/anniekobia/androidlinting/MainActivity.kt
@@ -22,6 +22,9 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.anniekobia.androidlinting.ui.theme.AndroidLintingTheme
 
+/**
+ * Entry point, displays success message text
+ */
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,7 +34,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background,
                 ) {
-                    Greeting(3, "\"Linting in Android\"")
+                    Greeting(number = 3, topic ="\"Linting in Android\"")
                 }
             }
         }
--- build.gradle.kts
+++ build.gradle.kts
@@ -3,4 +3,5 @@ plugins {
     id("com.android.application") version "8.2.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("org.jlleitschuh.gradle.ktlint") version "12.1.0"
+    id("io.gitlab.arturbosch.detekt") version "1.23.5"
 }
